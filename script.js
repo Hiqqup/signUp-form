20aab24cf6f2812d51723bbc0b1958d693e07cbb
hiqqup <jubu6000@gmail.com>
2023-07-29 15:11:06 +0200

    basic styling & js functionality

--- script.js
+++ script.js
@@ -1,3 +1,52 @@
+const fieldsets = document.querySelectorAll('fieldset');
+const nextButton = document.querySelector('#next-button');
+const previousButton = document.querySelector('#previous-button');
+let position = 0;
+fieldsets[position].classList.add('enabled')
+
+nextButton.addEventListener("click",()=> crementPosition(1));
+previousButton.addEventListener("click",()=> crementPosition(-1));
+
+function crementPosition(num){
+	position+= num;
+	enableCurrentPosition(position);
+	switch (position){
+		case 0: 
+			previousButton.disabled = true;
+		break;
+		case 1:
+			previousButton.disabled = false;
+		break;
+		case fieldsets.length -2:
+			nextButton.disabled = false;
+		break;
+		case fieldsets.length -1: 
+			nextButton.disabled = true;
+		break;
+	}
+
+}
+function enableCurrentPosition(position){
+	fieldsets.forEach(fieldset => {
+		fieldset.classList.remove('enabled');
+	});
+	fieldsets[position].classList.add('enabled');
+}
+const file = document.querySelector("#profile-picture");
+const imgPreview = document.querySelector('#picture-preview')
+
+file.addEventListener("change",() => getImgData());
+function getImgData() {
+  const files = file.files[0];
+  if (files) {
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(files);
+    fileReader.addEventListener("load", function () {
+      imgPreview.src =this.result;
+    });    
+  }
+}
+
 const input = document.querySelector('#user-points');
 const output = document.querySelector('#user-points-output');
 const meter = document.querySelector('#user-points-meter')
@@ -5,5 +54,10 @@ input.addEventListener('input', ()=>{
 	output.value = input.value
 	meter.value = input.value;
 });
+const colorContainer = document.querySelector('.color-container')
+const colorPicker = document.querySelector('#color');
 
+colorPicker.addEventListener('input', ()=>{
+    colorContainer.style.backgroundColor = colorPicker.value;
+});
 
